1be43d2ef25ece14bbf8ba737a74519ce18646ce
Willow <23151976+3intermute@users.noreply.github.com>
2023-05-22 00:50:56 -0700

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- driver/.pswap.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/mnt/driver/pswap.ko := ld -r -m elf_x86_64 -z noexecstack --build-id=sha1  -T scripts/module.lds -o /mnt/driver/pswap.ko /mnt/driver/pswap.o /mnt/driver/pswap.mod.o;  true
--- driver/.pswap.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/mnt/driver/pswap.mod := printf '%s\n'   pswap.o | awk '!x[$$0]++ { print("/mnt/driver/"$$0) }' > /mnt/driver/pswap.mod
